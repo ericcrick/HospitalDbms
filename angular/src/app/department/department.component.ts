0e2bba0422bf55b2c798796407f2aa7eca604bed
ericnanaosei <eric.nana-osei@afrilogicsolutions.com>
2022-11-15 08:06:52 +0000

    add department list

--- angular/src/app/department/department.component.ts
+++ angular/src/app/department/department.component.ts
@@ -1,15 +1,27 @@
+import { ListService, PagedResultDto } from '@abp/ng.core';
 import { Component, OnInit } from '@angular/core';
+import { DepartmentDto } from '@proxy/departments/dto';
+import { DepartmentService } from '@proxy/departments/service';
 
 @Component({
   selector: 'app-department',
   templateUrl: './department.component.html',
-  styleUrls: ['./department.component.scss']
+  styleUrls: ['./department.component.scss'],
+  providers: [ListService]
 })
 export class DepartmentComponent implements OnInit {
-
-  constructor() { }
+  department = { items: [], totalCount: 0 } as PagedResultDto<DepartmentDto>
+  constructor(
+    public readonly list: ListService, 
+    private departmentService:DepartmentService
+    ) { }
 
   ngOnInit(): void {
+    const departmentStreamCreator = (query) => this.departmentService.getList(query);
+
+    this.list.hookToQuery(departmentStreamCreator).subscribe((response) => {
+      this.department = response;
+    });
   }
 
 }

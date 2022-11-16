7bbb13b7886820cc42c8d5e9ff0b18d2dcea80dd
ericnanaosei <eric.nana-osei@afrilogicsolutions.com>
2022-11-16 06:48:14 +0000

    updating dashboard with total number of records

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { AuthService } from '@abp/ng.core';
 import { Component, OnInit } from '@angular/core';
 import { DepartmentService } from '@proxy/departments/service';
 import { OAuthService } from 'angular-oauth2-oidc';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -12,13 +13,14 @@ export class HomeComponent implements OnInit {
   get hasLoggedIn(): boolean {
     return this.oAuthService.hasValidAccessToken();
   }
-
+  totalDepartmentRecord: Observable<number>;
   constructor(
     private oAuthService: OAuthService, 
     private authService: AuthService, 
     private departmentService:DepartmentService,
     ){}
   ngOnInit(): void {
+    this.totalDepartmentRecord = this.departmentService.getDepartmentRecordCount();
   }
 
   login() {
